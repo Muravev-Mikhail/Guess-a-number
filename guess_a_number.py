8d076e850b9a517f5c0f8e832c7b039392ae86b4
Muravev-Mikhail <mmuravev2008@gmail.com>
2026-07-23 00:19:09 +0300

    Угадай число 1.0

---
+++ guess_a_number.py
@@ -0,0 +1,14 @@
+from random import *
+print('\n\n Угадай число от 1 до 100!!!')
+a = randint(1,100)
+while True:
+    b = int(input())
+    if a==b:
+        print('Ты угадал! Это число', a)
+        break
+    if a<b:
+        print('Бери меньше\n')
+    if a>b:
+        print('Бери больше\n')
+
+
